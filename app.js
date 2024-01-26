e4b104fb8afe4f07cf41e6d49c13fab1e1e990ef
Higor Gabriel Lino Silva <65170741+H1gor1@users.noreply.github.com>
2024-01-26 17:08:13 -0300

    added cors

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Importe o pacote cors
 const Melon = require('./index');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para analisar o corpo da solicitação como JSON
 app.use(bodyParser.json());
 
+// Middleware para lidar com o CORS
+app.use(cors());
+
 // Rota para obter o chart em tempo real
 app.get('/api/melon/realtime', (req, res) => {
-  const melon = new Melon(null, {   cutLine: 100 });
+  const melon = new Melon(null, { cutLine: 100 });
   melon.realtime().then(data => res.json(data)).catch(err => res.status(500).json({ error: err.message }));
 });
 
 // Rota para obter o chart diário
 app.get('/api/melon/daily', (req, res) => {
-  const melon = new Melon(null, { cutLine: 100 }); // Certifique-se de passar a opção cutLine corretamente
+  const melon = new Melon(null, { cutLine: 100 });
   melon.daily().then(data => res.json(data)).catch(err => res.status(500).json({ error: err.message }));
 });
 
